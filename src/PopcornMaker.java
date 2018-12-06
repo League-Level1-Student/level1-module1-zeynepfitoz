6fa8d4e8bbd5d9f5d3939004cfbefa151cf9a4b4
U have'nt <whaaqaaaidchbfgvruihgiudfgiufhjifo@gmail.com>
2018-12-05 18:31:00 -0800

    popcorn

--- src/PopcornMaker.java
+++ src/PopcornMaker.java
@@ -1,6 +1,11 @@
+import javax.swing.JOptionPane;
 
 public class PopcornMaker {
 public static void main(String[] args) {
-	
+	String flavor = JOptionPane.showInputDialog("What flavor popcorn do you want?");
+	Popcorn pop =  new Popcorn(flavor);	
+	Microwave micro = new Microwave();
+	micro.putInMicrowave(pop);
+	String cooking = JOptionPane.showInputDialog("How long do you want to cook the popcorn?");
 }
 }
